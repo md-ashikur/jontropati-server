b4a4f0ad4d6fe8acd9d99cf2811fc5bb0923481e
MD Ashikur Rahman <71284265+WGDAshik@users.noreply.github.com>
2022-05-26 13:11:47 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const cors =require('cors');
+const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,22 +14,36 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.krreql1.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-try{
-  await client.connect();
-  const itemsCollection = client.db('jontropati').collection('items');
+async function run() {
+  try {
+    await client.connect();
+    const itemsCollection = client.db('jontropati').collection('items');
+    const userCollection = client.db('jontropati').collection('user');
 
 
-  app.get('/item', async(req, res) =>{
-    const query = {};
-    const cursor = itemsCollection.find(query);
-    const items = await cursor.toArray();
-    res.send(items);
-  })
-}
-finally{
+    app.get('/item', async (req, res) => {
+      const query = {};
+      const cursor = itemsCollection.find(query);
+      const items = await cursor.toArray();
+      res.send(items);
+    })
 
-}
+    // user====================
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateJontro = { $set: user, };
+      const result = await userCollection.updateOne(filter, updateJontro, options);
+
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ result, token });
+    })
+  }
+  finally {
+
+  }
 }
 run().catch(console.dir);
 
