cde79f896685bfbce96b59f0f71ff3921bad2e59
MD Ashikur Rahman <71284265+WGDAshik@users.noreply.github.com>
2022-05-26 11:44:28 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors =require('cors');
 require('dotenv').config();
 
@@ -7,6 +8,31 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.krreql1.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+try{
+  await client.connect();
+  const itemsCollection = client.db('jontropati').collection('items');
+
+
+  app.get('/item', async(req, res) =>{
+    const query = {};
+    const cursor = itemsCollection.find(query);
+    const items = await cursor.toArray();
+    res.send(items);
+  })
+}
+finally{
+
+}
+}
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
