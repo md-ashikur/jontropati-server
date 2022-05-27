161b7bdf2356ad3e48a2fa9b3f40f44ca3cfb056
MD Ashikur Rahman <71284265+WGDAshik@users.noreply.github.com>
2022-05-27 12:23:35 +0600

    all user api

--- index.js
+++ index.js
@@ -14,6 +14,23 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.krreql1.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized Access' });
+  }
+
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (error, decoded) {
+    if (error) {
+      return res.status(403).send({ message: 'Forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+
 async function run() {
   try {
     await client.connect();
@@ -27,6 +44,48 @@ async function run() {
       const items = await cursor.toArray();
       res.send(items);
     })
+
+    // verifytoken===================
+    app.get('/user', verifyJWT, async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+
+    // ====================all users in admin page================
+    app.get('/user', async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    })
+
+    // find admin==============
+    app.get('/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === 'admin';
+      res.send({ admin: isAdmin });
+    })
+
+    // admin====================
+    app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const adminRequester = req.decoded.email;
+
+      const requesterAccount = await userCollection.findOne({ email: adminRequester });
+      if (requesterAccount.role === 'admin') {
+        const filter = { email: email };
+        const updateJontro = {
+          $set: { role: 'admin' }
+        };
+        const result = await userCollection.updateOne(filter, updateJontro);
+
+        res.send(result);
+      }
+      else {
+        res.status(403).send({ message: 'forbidden' });
+      }
+
+    })
+
 
     // user====================
     app.put('/user/:email', async (req, res) => {
